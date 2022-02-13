f24c16a38938d40cc0b1c2558b1675f2cd413321
vovapasko <vovapasko1699@gmail.com>
2022-02-13 15:36:02 +0100

    provided payment on each floor

--- ParkingLot/ParkingFloor.cs
+++ ParkingLot/ParkingFloor.cs
@@ -4,9 +4,14 @@ namespace OopDesigns.ParkingLot
     public class ParkingFloor
     {
         List<ParkingSpot> parkingSpots;
+        private IPayable customersPortal;
         public bool HasFreePlaces()
         {
             return true;
+        }
+        public ParkingTicket PayForParking(Customer customer)
+        {
+            return customersPortal.PayForParking(customer.Ticket);
         }
     }
 }
